192b2c878c822bb3c32b880275933a557cdf70c0
raja <102196806+SMRPcoder@users.noreply.github.com>
2023-07-22 19:55:39 +0530

    nav and style

--- src/Hooks/OpenRoutes.js
+++ src/Hooks/OpenRoutes.js
@@ -7,7 +7,13 @@ const OpenContext=createContext();
 const OpenRoutes=({url})=>{
     const navigate=useNavigate();
 console.log(url);
+const token=localStorage.getItem("token");
+useEffect(()=>{
+    if(token?.length>0){
+        navigate("/admin/dashboard")
+    }
 
+})
 
     return(
         <OpenContext.Provider value={{url}}>
--- src/Hooks/useTable.js
+++ src/Hooks/useTable.js
@@ -11,37 +11,45 @@ const TableComponent = ({ columns, data }) => {
   } = useTable({ columns, data });
 
   return (
-    <div className=' text-center justify-center flex mt-60'>
-    <table {...getTableProps()} >
-      <thead className='border border-orange-500' >
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()} className='border border-orange-500' >
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()} style={{ padding: '8px', border: '1px solid black' }} className='text-orange-500'>
-                {column.render('Header')}
-              </th>
+<div className="flex flex-col bg-white">
+    <div className="overflow-x-auto">
+        <div className="p-1.5 w-full inline-block align-middle">
+            <div className="overflow-hidden border rounded-lg">
+            <table {...getTableProps()} className="min-w-full divide-y divide-gray-200" >
+            <thead className="bg-gray-50" >
+            {headerGroups.map((headerGroup) => (
+               <tr {...headerGroup.getHeaderGroupProps()} >
+               {headerGroup.headers.map((column) => (
+                 <th {...column.getHeaderProps()} style={{ padding: '8px', border: '1px solid black' }} >
+                   {column.render('Header')}
+                 </th>
+               ))}
+             </tr>
             ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()} className='border border-orange-500' >
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()} className='border border-orange-500 hover:bg-white hover:text-black' >
-              {row.cells.map((cell) => {
-                return (
-                  <td {...cell.getCellProps()} style={{ padding: '8px', border: '1px solid black' }} className='text-orange-500' >
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                    </thead>
+                    <tbody {...getTableBodyProps()} className="divide-y divide-gray-200" >
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} className='border border-orange-500 hover:bg-white hover:text-black' >
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()} style={{ padding: '8px', border: '1px solid black' }} className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap" >
+                        {cell.render('Cell')}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+
+                </table>
+            </div>
+        </div>
     </div>
+    </div>
+
   );
 };
 
