18b5c457ad290a8ce86935aaae575749789d8799
raja <102196806+SMRPcoder@users.noreply.github.com>
2023-07-23 15:02:52 +0530

    new update

--- src/Hooks/useTable.js
+++ src/Hooks/useTable.js
@@ -11,12 +11,7 @@ const TableComponent = ({ columns, data }) => {
   } = useTable({ columns, data });
 
   return (
-<<<<<<< HEAD
-
-    <div className="flex flex-col bg-white">
-=======
 <div className="flex flex-col bg-white">
->>>>>>> d6af87a625f0c58d9be2968c1ec8f9cc5aea71d2
     <div className="overflow-x-auto">
         <div className="p-1.5 w-full inline-block align-middle">
             <div className="overflow-hidden border rounded-lg">
@@ -48,93 +43,7 @@ const TableComponent = ({ columns, data }) => {
               );
             })}
           </tbody>
-<<<<<<< HEAD
-                    {/* <tbody >
-                        <tr>
-                            <td >
-                                1
-                            </td>
-                            <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                Jone Doe
-                            </td>
-                            <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                jonne62@gmail.com
-                            </td>
-                            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                <a
-                                    className="text-green-500 hover:text-green-700"
-                                    href="#"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                <a
-                                    className="text-red-500 hover:text-red-700"
-                                    href="#"
-                                >
-                                    Delete
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                                2
-                            </td>
-                            <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                Jone Doe
-                            </td>
-                            <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                jonne62@gmail.com
-                            </td>
-                            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                <a
-                                    className="text-green-500 hover:text-green-700"
-                                    href="#"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                <a
-                                    className="text-red-500 hover:text-red-700"
-                                    href="#"
-                                >
-                                    Delete
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                                3
-                            </td>
-                            <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                Jone Doe
-                            </td>
-                            <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                                jonne62@gmail.com
-                            </td>
-                            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                <a
-                                    className="text-green-500 hover:text-green-700"
-                                    href="#"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                            <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                                <a
-                                    className="text-red-500 hover:text-red-700"
-                                    href="#"
-                                >
-                                    Delete
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody> */}
-=======
 
->>>>>>> d6af87a625f0c58d9be2968c1ec8f9cc5aea71d2
                 </table>
             </div>
         </div>
--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -19,102 +19,7 @@ export default function Login() {
         <div>
         <div className=' text-center mt-[100px] p-4'>
             <h1 className=' font-bold text-2xl text-orange-500' >Login Form</h1>
-
-<<<<<<< HEAD
-            </div>
-
-            <div className=' text-center pl-[200px] pr-[200px] '>
-                <Formik initialValues={{
-                    email: "",
-                    password: ""
-                }}
-                    validationSchema={loginSchema}
-                    onSubmit={(values) => {
-                        // console.log(values)
-                        axios.post(`${url}/user/login`, {
-                            ...values
-                        }).then(data => {
-                            if (data.data.status) {
-                                console.log(data.data);
-                                localStorage.setItem("token", data.data.token);
-                                localStorage.setItem("customData", JSON.stringify({token:data.data.token}));
-
-                                Notify.success("Login Successfull");
-                                setTimeout(() => {
-                                    navigate("/admin/dashboard")
-                                }, 1000)
-
-                            } else {
-                                Notify.failure(data.data.message);
-                            }
-                        })
-                    }}
-                >
-                    {({ errors, touched }) => (
-                       <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-                       <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                           <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
-                              Sign in
-                           </h1>
-                           <Form className="mt-6" >
-                               <div className="mb-2">
-                                   <label
-                                       for="email"
-                                       className="block text-sm font-semibold text-gray-800"
-                                   >
-                                       Email
-                                   </label>
-                                   <Field 
-                                       className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                                    name="email" />{errors.email && touched.email ? <div className=' text-red-500'><p>{errors.email}</p></div> : null}
-                       
-                               </div>
-                               <div className="mb-2">
-                                   <label
-                                       for="password"
-                                       className="block text-sm font-semibold text-gray-800"
-                                   >
-                                       Password
-                                   </label>
-                                   <Field 
-                                   name="password"
-                                   type="password"
-                                       className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                       
-                                    />{errors.password && touched.password ? <div className=' text-red-500'><p>{errors.password}</p></div> : null}
-                       
-                               </div>
-                               <a
-                                   href="#"
-                                   className="text-xs text-purple-600 hover:underline"
-                               >
-                                   Forget Password?
-                               </a>
-                               <div className="mt-6">
-                                   <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                                       Login
-                                   </button>
-                               </div>
-                               </Form>
-                           <p className="mt-8 text-xs font-light text-center text-gray-700">
-                               {" "}
-                               Don't have an account?{" "}
-                               <a
-                                   href="#"
-                                   className="font-medium text-purple-600 hover:underline"
-                               >
-                                   Sign up
-                               </a>
-                           </p>
-                       </div>
-                       </div>
-                    )}
-
-                </Formik>
-            </div>
-=======
->>>>>>> d6af87a625f0c58d9be2968c1ec8f9cc5aea71d2
-        </div>
+            
 
         <div className=' text-center pl-[200px] pr-[200px] '>
             <Formik initialValues={{
@@ -205,6 +110,7 @@ export default function Login() {
 
             </Formik>
         </div>
+    </div>
     </div>
     )
 }
